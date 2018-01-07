1b812b8b01ba3b998fc035d8aca3649592f8ef72
xlualy <zuluparvi@gmail.com>
2018-01-07 15:43:26 +0500

    testing docker deployment

--- src/main/java/com/example/demo/resource/HelloResource.java
+++ src/main/java/com/example/demo/resource/HelloResource.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by Zul Qarnain on 1/3/2018.
  */
 @RestController
-@RequestMapping("/rest/docker/hello")
+@RequestMapping("")
 public class HelloResource {
     @GetMapping
     public String hello(){
-        return "hello Youtube";
+        return "  youtube";
     }
 }
